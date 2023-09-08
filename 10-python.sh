c9ab49008ffe049e23c731260635b471e117d79b
shc <sh@c>
2023-09-08 05:39:58 +0200

    Rework python-related

--- 10-python.sh
+++ 10-python.sh
@@ -1,14 +1,24 @@
 pacman -S --needed \
-    python python-pip python-pipx \
-    python-black pycharm-community-edition
+    python python-pip python-pipx python-poetry \
+    pycharm-community-edition
 
 # // run as user, NOT root!
 # $ pipx install "black[d]"
 
 # pycharm-config:
-#   1. theme (dark purple), ui-font: avenir; editor-font: source-code-pro
-#   2. plugin: ideavim, black
-#       a. detect blackd location
-#   3. bind: Settings->Keymap
-#       a. Alt-Shift-Enter
-#       -> Run/Run/Debug/<logo>Run
+# 1. install plugin
+#   dard-purple
+#   ideavim
+#   black-connect
+# 2. configure:
+#   a. visual:
+#       Settings->Appearance&Behavior->Appearance->Theme: dark purple
+#       Settings->Appearance&Behavior->Appearance->Use custom font: Avenir
+#       Settings->Editor->Font: Shevska
+#   b. blackd:
+#       Settings->Tools->BlackConnect:
+#           Detect Path for blackd
+# 3. bind:
+#   Settings->Keymap:
+#   Run->Run/Debug->Run: Alt-Shift-Enter
+# 4. restart pycharm
