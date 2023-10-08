1d75a70a7eab387d35898054d94978a0bf11157e
shc <sh@c>
2023-10-09 00:50:12 +0200

    Rework zfs-hand'g with zen&lts

--- zen/get.sh
+++ zen/get.sh
@@ -1,14 +1,27 @@
+function is_installed() {
+    pacman -Qs "${1}" >/dev/null
+}
+
 function kernel_version_required() {
-    local mode
+    local package="zfs-linux-${2}"
+
+    function __f() {
+        pacman -"${1}"i "${package}" | \
+        grep "^Depends On" | \
+        sed "s/.*linux-${2}=\\(\\S*\\).*/\\1/"
+    }
+
     if [[ "${1}" == "remote" ]]; then
-        mode="S"
+        __f "S" "${2}"
     elif [[ "${1}" == "local" ]]; then
-        mode="Q"
+        if is_installed "${package}"; then
+            __f "Q" "${2}"
+        else
+            echo "placeholder"
+        fi
     fi
 
-    pacman -"${mode}"i zfs-linux-"${2}" | \
-    grep "^Depends On" | \
-    sed "s/.*linux-${2}=\\(\\S*\\).*/\\1/"
+    unset -f __f
 }
 
 function need_update() {
@@ -20,39 +33,73 @@ function need_update() {
 }
 
 function download() {
-    for f in "${@:2}"; do
-        if [ ! -f "${f}" ]; then
-            wget "${1}/${f}"
-        fi
+    local file="${1}-${2}-x86_64.pkg.tar.zst"
+    if [ ! -f "${file}" ]; then
+        wget "https://archive.archlinux.org/packages/${1[1]}/${1}/${file}"
+    fi
+}
+
+function download_packages_kernel() {
+    for p in "${@:2}"; do
+        download "${p}" "${1}"
     done
 }
 
 function uninstall_if_installed() {
     for p in "${@}"; do
-        if pacman -Qi "${p}" >/dev/null; then
+        if is_installed "${p}"; then
             sudo pacman -Rns "${p}"
         fi
     done
 }
 
-function pipeline_zen() {
-    local suffix="-x86_64.pkg.tar.zst"
-    local prefixes=("linux-zen" "linux-zen-headers" "linux-zen-docs")
-    local version
-    version="$(kernel_version_required remote zen)"
-    local packages_kernel=()
-    for prefix in "${prefixes[@]}"; do
-        packages_kernel+=("${prefix}-${version}${suffix}")
+function pipeline() {
+    local kernel install_zfs_header=false
+    while (( ${#} > 0 )); do
+        case "${1}" in
+            "-k" | "--kernel" )
+                kernel="${2}"
+                shift; shift
+                ;;
+            "--install-zfs-header" )
+                install_zfs_header=true
+                shift
+                ;;
+        esac
     done
 
-    download "https://archive.archlinux.org/packages/l/linux-zen" "${packages_kernel[@]}"
-    local packages_zfs=("zfs-linux-zen" "zfs-linux-zen-headers")
+    local suffix="-x86_64.pkg.tar.zst"
+    local packages_kernel=("linux-${kernel}" "linux-${kernel}-headers" "linux-${kernel}-docs")
+    local packages_kernel_path=()
+    local version
+    version=$(kernel_version_required remote "${kernel}")
 
-    if need_update zen; then
+    download_packages_kernel "${version}" "${packages_kernel[@]}"
+    for package in "${packages_kernel[@]}"; do
+        packages_kernel_path+=("${package}-${version}${suffix}")
+    done
+
+    local packages_zfs=("zfs-linux-${kernel}")
+    if ${install_zfs_header}; then
+        packages_zfs+=("zfs-linux-${kernel}-headers")
+    fi
+
+    if need_update "${kernel}"; then
         uninstall_if_installed "${packages_zfs[@]}" "${packages_kernel[@]}"
 
-        sudo pacman -U "${packages_kernel[@]}"
+        sudo pacman -U "${packages_kernel_path[@]}"
         sudo pacman -S "${packages_zfs[@]}"
     fi
 }
-pipeline_zen
+
+function main() {
+    case "${1}" in
+        "zen" | "lts" )
+            pipeline -k "${1}";;
+        * )
+            pipeline -k "zen"
+            pipeline -k "lts"
+            ;;
+    esac
+}
+main "${@}"
