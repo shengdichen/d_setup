00b1181827e19e914604f8fcb03d8e8542dbc3a9
shc <sh@c>
2023-10-25 02:05:36 +0200

    Rewrite 02-graphic.sh

--- 02-graphic.sh
+++ 02-graphic.sh
@@ -1,31 +1,50 @@
-pacman -S --needed \
-    adobe-source-code-pro-fonts \
-    adobe-source-han-sans-otc-fonts \
-    adobe-source-han-serif-otc-fonts \
-    libertinus-font \
-    ttf-fira-code \
-    noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji \
-    font-manager
+source "./util.sh"
 
-pacman -S --needed \
-    wl-clipboard xorg-xwayland \
-    sway swaylock swaybg xdg-desktop-portal-wlr \
-    grim slurp wf-recorder capitaine-cursors light gammastep
-pacman -S --needed \
-    alacritty foot \
-    tmux vifm fzf the_silver_searcher
-pacman -S --needed \
-    sshfs exfatprogs dosfstools \
-    pcmanfm-gtk3 gvfs gvfs-mtp gvfs-afc
+function __base() {
+    install "arch" \
+        alacritty foot \
+        tmux vifm neovim vi fzf the_silver_searcher
 
-pacman -S --needed \
-    zathura zathura-pdf-mupdf zathura-djvu zathura-ps
-pacman -S --needed \
-    pulsemixer mpv \
-    sox cmus mpd mpc ncmpc \
-    imv yt-dlp ytfzf
-pacman -S --needed \
-    firefox-developer-edition chromium w3m \
-    qutebrowser python-adblock \
-    transmission-cli deluge-gtk
+    install "arch" \
+        sshfs exfatprogs dosfstools \
+        pcmanfm-gtk3 gvfs gvfs-mtp gvfs-afc
+}
 
+function __wm() {
+    install "arch" \
+        wl-clipboard xorg-xwayland \
+        sway swaylock swaybg xdg-desktop-portal-wlr \
+        grim slurp wf-recorder capitaine-cursors light gammastep
+
+    install "aur" \
+        wdisplays
+
+    install "arch" \
+        adobe-source-code-pro-fonts \
+        adobe-source-han-sans-otc-fonts \
+        adobe-source-han-serif-otc-fonts \
+        libertinus-font \
+        ttf-fira-code \
+        noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji \
+        font-manager
+}
+
+function __office() {
+    install "arch" \
+        zathura zathura-pdf-mupdf zathura-djvu zathura-ps
+}
+
+function __media() {
+    install "arch" \
+        pulsemixer mpv \
+        sox cmus mpd mpc ncmpc \
+        imv yt-dlp ytfzf
+
+    install "arch" \
+        firefox-developer-edition chromium w3m \
+        qutebrowser python-adblock \
+        transmission-cli deluge-gtk
+
+    install "aur" \
+        wdisplays
+}
