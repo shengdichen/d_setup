3f826e1d9c79d60c78cae4024783d0fcc3a3b9ab
shc <sh@c>
2023-10-28 14:47:42 +0200

    Add more mail setup

--- 01-mail.sh
+++ 01-mail.sh
@@ -14,6 +14,58 @@ function __dot() {
     (cd "$(dot_dir)" && clone_and_stow "$(clone_url github ${base})" "${base}")
 }
 
+function __dot_dir() {
+    echo "$(dot_dir)/d_mail/"
+}
+
+function create_box() {
+    function __f() {
+        local maildir=false dirs
+        while (( ${#} > 0 )); do
+            case "${1}" in
+                "-m" )
+                    maildir=true
+                    shift ;;
+                "--" )
+                    dirs=("${@:2}")
+                    break
+            esac
+        done
+
+        for d in "${dirs[@]}"; do
+            mkdir -p "${d}"
+            chmod 700 "${d}"
+            if "${maildir}"; then
+                for maild in "cur" "new"; do
+                    mkdir -p "${d}/${maild}"
+                done
+            fi
+        done
+    }
+
+    local boxes_raw=() boxes_maildir=() box_dir
+    box_dir="$(__dot_dir)/.local/share/mail/"
+
+    for d in "eth" "gmail" "outlook"; do
+        boxes_raw+=("${box_dir}/raw/${d}")
+    done
+    __f -- "${boxes_raw[@]}"
+
+    for d in "xyz/.INBOX" "xyz/.Sent"; do
+        boxes_maildir+=("${box_dir}/raw/${d}")
+    done
+    for d in "draft" "hold" "trash" "x"; do
+        boxes_maildir+=("${box_dir}/all/.${d}")
+    done
+    __f -m -- "${boxes_maildir[@]}"
+
+    unset -f __f
+}
+
+function fdm_conf() {
+    chmod 600 "$(__dot_dir)/.config/fdm/config"
+}
+
 function __extra() {
     # 0. manually setup protonbridge:
     #   login;
