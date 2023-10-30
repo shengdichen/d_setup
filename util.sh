4ef5672b694d268222d7c6f27f8863fd871f8dac
shc <sh@c>
2023-10-30 10:22:38 +0100

    Rewrite aur-install

--- util.sh
+++ util.sh
@@ -18,23 +18,30 @@ function __sudo() {
     echo "${SUDO}"
 }
 
+function install_arch_cache() {
+    for p in "${@}"; do
+        echo "Installing [ARCH-CACHE] ${p}"
+        "$(__sudo)" pacman -U --needed "${p}"
+    done
+}
+
 function install_aur() {
     (
         cd "$(bin_dir)" || exit
-        git clone "https://aur.archlinux.org/$1.git"
+        clone aur "${1}"
     )
 
     (
-        cd "$(bin_dir)/$1" || exit
+        cd "$(bin_dir)/${1}" || exit
         makepkg -src
 
         echo
-        echo "select package to install from:"
-        ls ./*".pkg.tar.zst"
-        echo ""
-
-        read -r pkg
-        "$(__sudo)" pacman -U "${pkg}"
+        echo "select package to install"
+        install_arch_cache "$(\
+            find . -maxdepth 1 -type f | \
+            grep "\.pkg\.tar\.zst$" | \
+            fzf --reverse --height=50%\
+        )"
     )
 }
 
@@ -47,7 +54,7 @@ function install() {
             "$(__sudo)" pacman -S --needed "${@:2}"
             ;;
         "arch-cache")
-            "$(__sudo)" pacman -U --needed "${@:2}"
+            install_arch_cache "${@:2}"
             ;;
         *)
             "$(__sudo)" pacman -S --needed "${@:2}"
@@ -55,12 +62,21 @@ function install() {
     esac
 }
 
-function clone_url() {
-    case "$1" in
+function clone() {
+    local repo link
+    case "${1}" in
         "github" | "GH" )
-            echo "git@github.com:shengdichen/$2.git"
+            repo=${2}
+            link="git@github.com:shengdichen/${repo}.git"
+            ;;
+        "aur" )
+            repo=${2}
+            link="https://aur.archlinux.org/${repo}.git"
             ;;
     esac
+    if [[ ! -d ${repo} ]]; then
+        git clone "${link}"
+    fi
 }
 
 function clone_and_stow() {
