fe1b52daa2eb28f3ebc994ff95c98eb98f33a59d
shc <sh@c>
2023-10-30 12:45:45 +0100

    Rework qutebrowser-setup

--- 20-qutebrowser.sh
+++ 20-qutebrowser.sh
@@ -4,23 +4,21 @@ function __install() {
     install "arch" \
         "qutebrowser" "tor"
     clone_and_stow self d_qutebrowser
-
-    systemctl enable --now "tor.service"
 }
 
-function __install_dict() {
-    (\
-        cd "/usr/share/qutebrowser/scripts/" && \
-        python "dictcli.py" install \
-            "de-DE" "en-US" "es-ES" "fr-FR" "it-IT" "pt-BR" "ru-RU" "sv-SE"\
-    )
+function __extra() {
+    systemctl enable --now "tor.service"
+
+    # download (offline) dictionaries
+    python "/usr/share/qutebrowser/scripts/dictcli.py" install \
+        "de-DE" "en-US" "es-ES" "fr-FR" "it-IT" "pt-BR" "ru-RU" "sv-SE"
 }
 
 function main() {
     __install
-    __install_dict
+    __extra
 
-    unfunction __install __install_dict
+    unfunction __install __extra
 }
 main
 unfunction main
