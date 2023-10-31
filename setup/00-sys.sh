c39390a43e6260f0c0b5e3bc18e503317d2ccc3a
shc <sh@c>
2023-10-31 09:06:11 +0100

    Initial-cleanup sys.sh

--- setup/00-sys.sh
+++ setup/00-sys.sh
@@ -4,40 +4,61 @@ function base() {
     install "arch" \
         base base-devel pacman-contrib \
         vi neovim \
+
+    install "arch" \
         man-db man-pages \
         man-pages-fr man-pages-de man-pages-ru man-pages-es \
         man-pages-sv man-pages-it man-pages-pt_br man-pages-zh_tw
 
-    # fs
     install "arch" \
-        exfatprogs \
         linux linux-headers linux-docs \
         linux-lts linux-lts-headers linux-lts-docs \
-        linux-firmware intel-ucode \
-        fwupd arch-install-scripts
+        linux-firmware
+
+    install "arch" \
+        lshw efibootmgr intel-ucode fwupd arch-install-scripts
+
+    install "arch" \
+        tlp acpi
+    # systemctl enable tlp && systemctl start tlp
+    # sudo tlp setcharge 59 60 BAT0
+
+    install "arch" \
+        ethtool smartmontools lsof \
+        exfatprogs openssh nfs-utils sshfs \
+        tar bzip2 bzip3 gzip xz zstd p7zip unrar zip unzip
+
+    install "arch" \
+        gnupg pass pass-otp zbar
+
+    install "arch" \
+        networkmanager \
+        networkmanager-openvpn networkmanager-openconnect nm-connection-editor \
+        tor nyx \
+        wget curl speedtest-cli rsync
+    # systemctl start tor
+
+    install "arch" \
+        bluez bluez-utils \
+        wireplumber \
+        pipewire pipewire-docs pipewire-alsa pipewire-pulse pipewire-jack \
+        gstreamer gstreamer-vaapi gst-libav gst-plugins-base gst-plugins-good
 }
 
-pacman -S --needed \
-    mesa vulkan-intel lib32-vulkan-intel \
-    vulkan-headers vulkan-tools intel-gpu-tools
-pacman -S --needed \
-    efibootmgr tlp acpi ethtool smartmontools lsof lshw \
-    openssh nfs-utils sshfs gnupg pass pass-otp zbar \
-    tar bzip2 bzip3 gzip xz zstd p7zip unrar zip unzip
+function graphics() {
+    install "arch" \
+        mesa vulkan-intel lib32-vulkan-intel \
+        vulkan-headers vulkan-tools intel-gpu-tools
 
-# systemctl enable tlp && systemctl start tlp
-# sudo tlp setcharge 59 60 BAT0
-pacman -S --needed \
-    networkmanager \
-    networkmanager-openvpn networkmanager-openconnect nm-connection-editor \
-    tor nyx \
-    wget curl speedtest-cli rsync
-# systemctl start tor
-pacman -S --needed \
-    bluez bluez-utils \
-    wireplumber \
-    pipewire pipewire-docs pipewire-alsa pipewire-pulse pipewire-jack \
-    gstreamer gstreamer-vaapi gst-libav gst-plugins-base gst-plugins-good
+    install "arch" \
+        vulkan-radeon lib32-vulkan-radeon \
+        libva-mesa-driver lib32-libva-mesa-driver \
+        mesa-vdpau lib32-mesa-vdpau \
+        radeontop
+
+    install "arch" \
+        amdvlk lib32-amdvlk
+}
 
 # pacman.conf {{{
 # 0. enable multilib
