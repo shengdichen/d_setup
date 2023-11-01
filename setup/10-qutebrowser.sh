373775b80d4b8a93ba5c280d306abf274eb85cdb
shc <sh@c>
2023-11-01 20:07:46 +0100

    Install qutebrowser-dict only if miss'g

--- setup/10-qutebrowser.sh
+++ setup/10-qutebrowser.sh
@@ -10,8 +10,18 @@ function __extra() {
     service_start -- tor
 
     # download (offline) dictionaries
-    python "/usr/share/qutebrowser/scripts/dictcli.py" install \
-        "de-DE" "en-US" "es-ES" "fr-FR" "it-IT" "pt-BR" "ru-RU" "sv-SE"
+    (
+        local _dict_dir="${HOME}/.local/share/qutebrowser/qtwebengine_dictionaries"
+        mkdir -p "${_dict_dir}"
+        cd "${_dict_dir}" || exit 3
+
+        for l in "de-DE" "en-US" "es-ES" "fr-FR" "it-IT" "pt-BR" "ru-RU" "sv-SE"; do
+            if ! find . -maxdepth 1 -printf "%P\n" | grep -q "^${l}-.*\.bdic$"; then
+                echo "[qutebrowser-dict:${l}] Installing"
+                python "/usr/share/qutebrowser/scripts/dictcli.py" install "${l}"
+            fi
+        done
+    )
 }
 
 function main() {
