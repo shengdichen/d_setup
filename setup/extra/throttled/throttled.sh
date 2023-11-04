af61b25d1dff95be96de069d5057baa6332bd186
shc <sh@c>
2023-11-04 10:50:50 +0100

    Add fzf-based throttled-conf select'n

--- setup/extra/throttled/throttled.sh
+++ setup/extra/throttled/throttled.sh
@@ -6,4 +6,12 @@ function __install() {
     service_start -- throttled
 }
 
+function __set_conf() {
+    echo "Select profile"
+    "$(__sudo)" cp \
+        "$(find . -maxdepth 1 -type f | grep "\.conf$" | fzf --height="37%")" \
+        "/etc/throttled.conf"
+}
+
 __install
+__set_conf
