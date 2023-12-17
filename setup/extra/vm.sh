2f7925fb60749b248118cff1fdb7b8258a6bc023
shc <sh@c>
2023-12-17 11:25:00 +0100

    feat(vm): setup

---
+++ setup/extra/vm.sh
@@ -0,0 +1,36 @@
+source "../util.sh"
+
+libvirt() {
+    # REF:
+    #   https://wiki.archlinux.org/title/Libvirt
+    install "arch" \
+        libvirt qemu-full \
+        virt-manager \
+        iptables-nft dnsmasq \
+        openbsd-netcat \
+        edk2-ovmf
+
+    printf "\n\n"
+    echo "Set UEFI:"
+    echo "    Edit -> Preferences -> x86 Firmware -> UEFI"
+    printf "Done "
+    read -r
+}
+
+libvirt_admin() {
+    local libvirt_gr="libvirt"
+    if ! id -nG "${USER}" | grep -qw "${libvirt_gr}"; then
+        printf "\n\n"
+        printf "Adding myself [%s] to %s-group: " "${USER}" "${libvirt_gr}"
+        read -r
+        "$(__sudo)" usermod -aG "${libvirt_gr}" "${USER}"
+
+        printf "\n\n"
+        printf "Must (re)login before using libvirt: "
+        read -r
+    fi
+
+    service_start -- libvirtd.service virtlogd.service
+}
+
+libvirt
