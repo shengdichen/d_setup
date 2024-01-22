c7e7c83181bb6e03349db747e64a43d5560083b7
shc <sh@c>
2024-01-22 23:45:21 +0100

    feat(pre-02): 1st batch cleanup

--- setup/pre/02.sh
+++ setup/pre/02.sh
@@ -7,103 +7,117 @@ MOUNT_MATRIX="m"
 DOT_ROOT="${HOME}/dot/dot"
 DOT_PRV="d_prv"
 
+__start() {
+    printf "%s> START " "${1}"
+}
+
+__error() {
+    printf "%s [exiting]\n" "${1}"
+    printf "\n"
+    exit 3
+}
+
+__done() {
+    printf "%s> DONE " "${1}" && read -r _
+    clear
+}
+
 _pre() {
     if [ "$(id -u)" -eq 0 ]; then
-        echo "Must be non-root, (create and) switch to user"
-        exit 3
+        __error "Must be non-root, (create and) switch to user"
     fi
 
-    sudo pacman -Syu
-    sudo pacman -S --needed \
-        fzf \
-        openssh git stow \
-        sshfs fuse2 unzip
+    if ! (sudo pacman -Syy &&
+        sudo pacman -S --needed \
+            fzf \
+            openssh git stow \
+            sshfs fuse2 unzip >/dev/null); then
+        __error "pacman-install failed; bad internet?"
+    fi
 
     if [ -d "${HOME}/.ssh" ]; then
         rm -r "${HOME}/.ssh"
     fi
-
-    if [ -d "${HOME}/dot" ]; then
-        (
-            cd "${DOT_ROOT}" || exit 3
-            if [ -d "${DOT_PRV}" ]; then
-                stow -D "${DOT_PRV}"
-            fi
-        )
+    if [ -d "${DOT_ROOT}/${DOT_PRV}" ]; then
+        (cd "${DOT_ROOT}" && stow -D "${DOT_PRV}")
     fi
     rm -rf "${HOME}/dot"
-
-    clear
 }
 
 raw_ssh() {
     printf "ssh-raw> start\n\n"
 
     if [ -d "${HOME}/.ssh" ]; then
-        echo "Found existing ssh-config, skipping"
+        printf "Found existing ssh-config, skipping\n"
         return
     fi
 
-    local zip_name=".ssh.zip"
-    if [ ! -f "${zip_name}" ]; then
-        local mount_tmp="${MOUNT_ROOT}/mount_tmp"
+    local mount_tmp="" disk=""
+    local ssh_zip=".ssh.zip"
+    if [ -f "${ssh_zip}" ]; then
+        printf "found existing [%s], using it" "${ssh_zip}"
+    else
+        mount_tmp="${MOUNT_ROOT}/mount_tmp"
         mkdir -p "${mount_tmp}"
 
-        local disk
-        echo "select source-disk"
-        disk="$(lsblk -o PATH,FSTYPE,SIZE,MOUNTPOINTS | fzf --reverse --height=30% | awk '{ print $1 }')"
+        printf "select source-disk\n"
+        disk="$(lsblk -o PATH,LABEL,FSTYPE,SIZE,MOUNTPOINTS | fzf --reverse --height=30% | awk '{ print $1 }')"
 
-        if sudo mount "${disk}" "${mount_tmp}"; then
-            if ! cp -f "${mount_tmp}/x/Dox/sys/${zip_name}" "${HOME}"; then
-                echo "[ssh-conf] not found on [${disk}], exiting"
-                exit 3
+        if ! sudo mount "${disk}" "${mount_tmp}"; then
+            __error "mount> [${disk}] failed"
+        else
+            if ! cp -f "${mount_tmp}/x/Dox/sys/${ssh_zip}" "${HOME}"; then
+                __error "[ssh-conf] not found on [${disk}]"
             fi
-        else
-            echo
-            echo "[ssh-conf] neither local nor on usb. Exiting"
-            echo
-            exit 3
         fi
-    fi
-
-    (
-        cd || exit 3
-        if unzip "${zip_name}"; then
-            rm "${zip_name}"
-            cd "./.ssh" && ${SHELL} "setup.sh"
-        else
-            echo "Wrong password for ssh, exiting"
-            exit 3
-        fi
-    )
-
-    echo
-    if sudo umount "${mount_tmp}"; then
-        rmdir "${mount_tmp}"
-        printf "Safely unmounted, remove storage now: "
-    else
-        printf "Unmount failed, you might be fine ignoring this though "
     fi
 
     printf "\n\n"
-    printf "ssh-raw> done " && read -r _ && clear
+
+    if ! (
+        cd || exit 3
+        if unzip "${ssh_zip}"; then
+            rm "${ssh_zip}"
+            cd "./.ssh" && ${SHELL} "setup.sh"
+        else
+            false
+        fi
+    ); then
+        __error "wrong password for ssh"
+    fi
+
+    printf "\n\n"
+
+    if [ "${mount_tmp}" ]; then
+        if sudo umount "${mount_tmp}"; then
+            rmdir "${mount_tmp}"
+            printf "umount-[%s]> success; remove storage now: " "${disk}"
+        else
+            lsblk
+            printf "\n\n"
+            printf "umount-[%s]> failed, you might be fine ignoring this though: " "${disk}"
+        fi
+        read -r _
+    fi
+
+    printf "\n\n"
+    __done "ssh-raw"
 }
 
 get_d_setup() {
-    printf "d_setup> start\n\n"
+    __start "d_setup"
 
-    local setup_link="shengdichen/d_setup.git"
-    (
+    local _link="shengdichen/d_setup.git"
+    if ! (
         cd || exit 3
-        if ! git clone "git@github.com:${setup_link}" dot; then
-            printf "\n\n"
-            printf "d_setup> clone: failed (bad internet?) "
-            exit 3
-        else
-            printf "\n\n"
-            printf "d_setup> done " && read -r _ && clear
+        if ! git clone "git@github.com:${_link}" dot; then
+            false
         fi
-    )
+    ); then
+        __error "clone: ${_link} (bad internet?)"
+    else
+        __done "d_setup"
+    fi
 }
 
 get_prv() {
@@ -175,9 +189,21 @@ _post() {
     fi
 }
 
-_pre
-raw_ssh
-get_d_setup
-get_prv
-_post
+case "${1}" in
+    "--clean")
+        _pre
+        ;;
+    "--full")
+        if _pre; then
+            clear
+            if raw_ssh && get_d_setup && get_prv; then
+                clear
+                _post
+            fi
+        fi
+        ;;
+    *)
+        printf "try --clean or --full\n"
+        ;;
+esac
 unset -f _pre raw_ssh get_d_setup get_prv _post
