83b1e28a56fec431197c74dc0791164f1b9fc0d2
shc <sh@c>
2024-02-09 20:20:08 +0100

    refactor(trackpoint): separate f'n for reload'g

--- setup/extra/trackpoint.sh
+++ setup/extra/trackpoint.sh
@@ -2,27 +2,58 @@
 
 . "../util.sh"
 
+MODULE="psmouse"
+
 __increase_refreshrate() {
     # REF:
     #   https://wiki.archlinux.org/title/Lenovo_ThinkPad_T480#TrackPoint_and_Touchpad
 
-    local _module="psmouse"
-    if systool -v -m "${_module}" | grep -q "synaptics_intertouch.*\"1\""; then
+    if systool -v -m "${MODULE}" | grep -q "synaptics_intertouch.*\"1\""; then
         return
     fi
 
     local _config="synaptics_intertouch=1"
-    # need to repeat this several times to take effect
-    for __ in $(seq 3); do
-        # for current boot
-        "$(__sudo)" modprobe -r "${_module}" && "$(__sudo)" modprobe "${_module}" "${_config}"
-        sleep 1
-    done
     # make permanent
-    "$(__sudo)" tee "/etc/modprobe.d/${_module}.conf" <<STOP >/dev/null
-options ${_module} ${_config}
+    "$(__sudo)" tee "/etc/modprobe.d/${MODULE}.conf" <<STOP >/dev/null
+options ${MODULE} ${_config}
 STOP
 }
 
-__increase_refreshrate
-unset -f __increase_refreshrate
+__reload() {
+    local _n_reloads=1 _time_sleep=5
+    while [ "${#}" -gt 0 ]; do
+        case "${1}" in
+            "--n-reloads")
+                _n_reloads="${2}"
+                shift && shift
+                ;;
+            "--time-sleep")
+                _time_sleep="${2}"
+                shift && shift
+                ;;
+            *)
+                exit 3
+                ;;
+        esac
+    done
+
+    for i in $(seq "${_n_reloads}"); do
+        printf "reloading> attempt [%s/%s]\n" "${i}" "${_n_reloads}"
+        "$(__sudo)" modprobe -r "${MODULE}" && "$(__sudo)" modprobe "${MODULE}"
+        if [ "${i}" -ne "${_n_reloads}" ]; then
+            sleep "${_time_sleep}"
+        fi
+    done
+}
+
+case "${1}" in
+    "--incr")
+        __increase_refreshrate
+        __reload --n-reloads 1
+        ;;
+    *)
+        __reload
+        ;;
+esac
+unset MODULE
+unset -f __increase_refreshrate __reload
