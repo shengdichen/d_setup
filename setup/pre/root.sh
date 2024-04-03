1315e48f7ac98ade363be59d47ff8108d86359a1
shc <sh@c>
2024-04-04 00:40:10 +0200

    feat(setup): rework locale-list of life

--- setup/pre/root.sh
+++ setup/pre/root.sh
@@ -314,23 +314,25 @@ localization() {
 
     if ! locale -a | grep "^sv_SE.utf8$" >/dev/null; then
         cat <<STOP >>/etc/locale.gen
+zh_CN.UTF-8 UTF-8
 en_US.UTF-8 UTF-8
-
-fr_CH.UTF-8 UTF-8
-fr_FR.UTF-8 UTF-8
-ru_RU.UTF-8 UTF-8
-ar_EG.UTF-8 UTF-8
-es_ES.UTF-8 UTF-8
-
-de_CH.UTF-8 UTF-8
 de_DE.UTF-8 UTF-8
+de_CH.UTF-8 UTF-8
+
+fr_FR.UTF-8 UTF-8
+fr_CH.UTF-8 UTF-8
 zh_HK.UTF-8 UTF-8
 zh_TW.UTF-8 UTF-8
-
-it_CH.UTF-8 UTF-8
+es_ES.UTF-8 UTF-8
+es_AR.UTF-8 UTF-8
 it_IT.UTF-8 UTF-8
-pt_BR.UTF-8 UTF-8
+it_CH.UTF-8 UTF-8
+
+ru_RU.UTF-8 UTF-8
 sv_SE.UTF-8 UTF-8
+ar_EG.UTF-8 UTF-8
+pt_BR.UTF-8 UTF-8
+ja_JP.UTF-8 UTF-8
 STOP
         locale-gen
     fi
