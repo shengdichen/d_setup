2a20a995abd231139cc616f416b26bdf4d545bad
shc <sh@c>
2024-05-02 03:00:04 +0200

    feat(nvidia): do NOT auto-update

--- setup/extra/nvidia.sh
+++ setup/extra/nvidia.sh
@@ -7,5 +7,30 @@ __base() {
         nvidia-dkms opencl-nvidia cuda
 }
 
-__base
-unset -f __base
+__update() {
+    __install arch -- nvidia-dkms
+}
+
+__reload() {
+    # REF:
+    #   https://stackoverflow.com/questions/43022843/nvidia-nvml-driver-library-version-mismatch
+
+    "$(__sudo)" modprobe -r nvidia_drm
+    "$(__sudo)" modprobe -r nvidia_modeset
+    "$(__sudo)" modprobe -r nvidia_uvm
+    "$(__sudo)" modprobe -r nvidia
+
+    nvidia-smi
+}
+
+case "${1}" in
+    "update")
+        __udpate
+        ;;
+    "reload")
+        __reload
+        ;;
+    *)
+        __base
+        ;;
+esac
