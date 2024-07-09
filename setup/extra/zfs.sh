97ba98c4e7c8f2e2a8faab44d987b690d1f6fa1b
shc <sh@c>
2024-07-09 17:38:56 +0200

    chore(extra): cleanup zfs install'n script

--- setup/extra/zfs.sh
+++ setup/extra/zfs.sh
@@ -1,98 +1,180 @@
-function __is_installed() {
-    pacman -Qs "${1}" >/dev/null
+#!/usr/bin/env bash
+
+. "../util.sh"
+
+__zfs_linux() {
+    printf "zfs-linux-%s" "${1}"
 }
 
-function __zfs() {
-    echo "zfs-linux-${1}"
-}
-
-function __linux_version_required() {
-    local flag
-    if [[ "${1}" == "remote" ]]; then
-        flag="S"
-    elif [[ "${1}" == "local" ]]; then
-        flag="Q"
-    fi
-
-    pacman -"${flag}"i "$(__zfs "${2}")" | \
-    grep "^Depends On" | \
-    sed "s/.*linux-${2}=\\(\\S*\\).*/\\1/"
-}
-
-function __need_install() {
-    if __is_installed "$(__zfs "${1}")"; then
-        [[
-            "$(__linux_version_required "remote" "${1}")" !=
-            "$(__linux_version_required "local" "${1}")"
-        ]]
-    else
-        true
-    fi
-}
-
-function __uninstall_if_installed() {
-    for p in "${@}"; do
-        if __is_installed "${p}"; then
-            sudo pacman -Rns --noconfirm "${p}"
-        fi
-    done
-}
-
-function pipeline() {
-    local kernel install_zfs_header=false
-    while (( ${#} > 0 )); do
+__pacman() {
+    __version() {
+        local _flag="Q"
         case "${1}" in
-            "-k" | "--kernel" )
-                kernel="${2}"
-                shift; shift
+            "--remote")
+                _flag="S"
+                shift
                 ;;
-            "--install-zfs-header" )
-                install_zfs_header=true
+            "--local")
+                _flag="Q"
                 shift
                 ;;
         esac
-    done
 
-    if __need_install "${kernel}"; then
-        __install "${kernel}" "${install_zfs_header}"
-    fi
-}
+        pacman -"${_flag}"i -- "${1}" |
+            grep "^Version\\s*:" |
+            sed "s/^Version\\s*:\\s*\\(\\S*\\)$/\\1/"
+    }
 
-function __install() {
-    local _linux=("linux-${1}" "linux-${1}-headers" "linux-${1}-docs")
-    local _zfs=("zfs-linux-${1}" "zfs-utils") _zfs_header="zfs-linux-${1}-headers"
+    __url() {
+        local _package="${1}" _version="${2}"
+        local _package_first_char
+        _package_first_char="$(printf "%s" "${_package}" | head -c 1)"
+        local _file="${_package}-${_version}-x86_64.pkg.tar.zst"
 
-    __uninstall_if_installed "${_zfs[@]}" "${_zfs_header}" "${_linux[@]}"
+        printf \
+            "https://archive.archlinux.org/packages/%s/%s/%s" \
+            "${_package_first_char}" \
+            "${_package}" \
+            "${_file}"
+    }
 
-    __install_linux "$(__linux_version_required remote "${1}")" "${_linux[@]}"
-    sudo pacman -S --noconfirm "${_zfs[@]}"
-    if ${2}; then
-        sudo pacman -S --noconfirm "${_zfs_header}"
-    fi
-}
+    __uninstall_safe() {
+        if [ "${1}" = "--" ]; then shift; fi
 
-function __install_linux() {
-    local urls=()
-    for p in "${@:2}"; do
-        urls+=("$(__url "${p}" "${1}")")
-    done
-    sudo pacman -U --noconfirm "${urls[@]}"
-}
+        local _packages=()
+        for _p in "${@}"; do
+            if __is_installed_arch "${_p}"; then
+                _packages+=("${_p}")
+            fi
+        done
 
-function __url() {
-    local f="${1}-${2}-x86_64.pkg.tar.zst"
-    echo "https://archive.archlinux.org/packages/${1[1]}/${1}/${f}"
-}
+        if [ "${#_packages[@]}" -ne 0 ]; then
+            sudo pacman -Rns --noconfirm -- "${_packages[@]}"
+        fi
+    }
 
-function main() {
+    __install_safe() {
+        if [ "${1}" = "--" ]; then shift; fi
+
+        local _packages=()
+        for _p in "${@}"; do
+            if ! __is_installed_arch "${_p}" || __is_outdated -- "${_p}"; then
+                _packages+=("${_p}")
+            fi
+        done
+
+        if [ "${#_packages[@]}" -ne 0 ]; then
+            sudo pacman -S --noconfirm -- "${_packages[@]}"
+        fi
+    }
+
+    __is_outdated() {
+        if [ "${1}" = "--" ]; then shift; fi
+        pacman -Qu -- "${1}" 1>/dev/null
+    }
+
     case "${1}" in
-        "zen" | "lts" )
-            pipeline -k "${1}";;
-        * )
-            for k in "zen" "lts"; do
-                pipeline -k "${k}"
-            done
+        "version")
+            shift
+            __version "${@}"
+            ;;
+        "url")
+            shift
+            __url "${@}"
+            ;;
+        "uninstall")
+            shift
+            __uninstall_safe "${@}"
+            ;;
+        "install")
+            shift
+            __install_safe "${@}"
+            ;;
+        "outdated")
+            shift
+            __is_outdated "${@}"
             ;;
     esac
 }
-main "${@}"
+
+pipeline() {
+    __linux_version_required() {
+        local _flag="S"
+        case "${1}" in
+            "--remote")
+                _flag="S"
+                shift
+                ;;
+            "--local")
+                _flag="Q"
+                shift
+                ;;
+        esac
+
+        pacman -"${_flag}"i "$(__zfs_linux "${1}")" |
+            grep "^Depends On" |
+            sed "s/.*linux-${1}=\\(\\S*\\).*/\\1/"
+    }
+
+    sudo pacman -Syy
+
+    local _packages_r=() _packages_u=() _packages_s=()
+    local _zfs_utils="zfs-utils" _zfs_linux
+    for _kernel in "zen" "lts"; do
+        _zfs_linux="$(__zfs_linux "${_kernel}")"
+        for _p in "${_zfs_utils}" "${_zfs_linux}"; do
+            if ! __is_installed_arch "${_p}"; then
+                _packages_s+=("${_p}")
+            else
+                if __pacman outdated "${_p}"; then
+                    _packages_r+=("${_p}")
+                    _packages_s+=("${_p}")
+                fi
+            fi
+        done
+
+        _version="$(__linux_version_required "${_kernel}")"
+        for _p in \
+            "linux-${_kernel}" \
+            "linux-${_kernel}-headers" \
+            "linux-${_kernel}-docs"; do
+            if ! __is_installed_arch "${_p}"; then
+                _packages_u+=("$(__pacman url "${_p}" "${_version}")")
+            elif ! [ "$(__pacman version "${_p}")" = "${_version}" ]; then
+                _packages_r+=("${_p}")
+                _packages_u+=("$(__pacman url "${_p}" "${_version}")")
+            fi
+        done
+    done
+
+    if [ ! "${#_packages_r[@]}" -eq 0 ]; then
+        sudo pacman -Rns --noconfirm -- "${_packages_r[@]}"
+    fi
+    if [ ! "${#_packages_u[@]}" -eq 0 ]; then
+        sudo pacman -U --noconfirm -- "${_packages_u[@]}"
+    fi
+    if [ ! "${#_packages_s[@]}" -eq 0 ]; then
+        sudo pacman -S --noconfirm -- "${_packages_s[@]}"
+    fi
+}
+
+__uninstall() {
+    for _kernel in "zen" "lts"; do
+        __pacman uninstall -- "$(__zfs_linux "${_kernel}")"
+
+        __pacman install -- \
+            "linux-${_kernel}" \
+            "linux-${_kernel}-headers" \
+            "linux-${_kernel}-docs"
+    done
+    __pacman uninstall -- "zfs-utils"
+}
+
+case "${1}" in
+    "uninstall")
+        __uninstall
+        ;;
+    *)
+        pipeline
+        ;;
+esac
