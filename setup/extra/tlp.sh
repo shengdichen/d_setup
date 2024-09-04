864cefc0d71ffe296658570887dc7aa6c8e143e7
shc <sh@c>
2024-09-04 16:38:59 +0200

    feat(tlp): impl't our own acpi-util

--- setup/extra/tlp.sh
+++ setup/extra/tlp.sh
@@ -2,6 +2,8 @@
 
 . "../util.sh"
 
+SYS_POWERSUPPLY="/sys/class/power_supply"
+
 __base() {
     __install arch -- \
         ethtool tlp acpi
@@ -47,6 +49,152 @@ set_threshold() {
     done
 }
 
-__base
-printf "\n"
-set_threshold "${@}"
+__calc() {
+    printf "%s \n" "${1}" | bc -l
+}
+
+__status_one() {
+    local _bat="${1}"
+
+    __battery_id() {
+        local _battery_id
+        _battery_id=$(($(printf "%s" "${_bat}" | tail -c "+4") + 1))
+        printf "b%s> " "${_battery_id}"
+    }
+
+    # NOTE:
+    #   unit: muWh
+    local _capacity_now _capacity_full _capacity_factory
+    _capacity_now="$(cat "${SYS_POWERSUPPLY}/${_bat}/energy_now")"
+    _capacity_full="$(cat "${SYS_POWERSUPPLY}/${_bat}/energy_full")"
+    _capacity_factory="$(cat "${SYS_POWERSUPPLY}/${_bat}/energy_full_design")"
+
+    local _power
+    _power="$(cat "${SYS_POWERSUPPLY}/${_bat}/power_now")" # muW
+
+    __health() {
+        local _n_cycles _perc
+        _n_cycles="$(cat "${SYS_POWERSUPPLY}/${_bat}/cycle_count")"
+        _perc="$(__calc "100 * ${_capacity_full} / ${_capacity_factory}")"
+        _wh="$(__calc "${_capacity_full} / (10 ^ 6)")"
+
+        printf "health: %.1fWh, %s->%.2f%%" "${_wh}" "${_n_cycles}" "${_perc}"
+    }
+
+    __perc() {
+        local _perc
+        _perc="$(__calc "100 * ${_capacity_now} / ${_capacity_full}")"
+
+        printf "%.3f%%" "${_perc}"
+    }
+
+    __in_use() {
+        [ "${_power}" -ne 0 ]
+    }
+
+    __powerdraw() {
+        local _power_watt
+        _power_watt="$(__calc "${_power} / (10 ^ 6)")"
+        printf "%.3fW" "${_power_watt}"
+    }
+
+    __time_to_empty() {
+        local _time
+        _time="$(__calc "60 * ${_capacity_now} / ${_power}")"
+
+        local _perc_per_ten
+        _perc_per_ten="$(__calc "100 * (1/6) * ${_power} / ${_capacity_full}")"
+
+        printf "(%.1fmin; %.1f%%/10min)" "${_time}" "${_perc_per_ten}"
+    }
+
+    __threshold() {
+        local _threshold_start="${SYS_POWERSUPPLY}/${_bat}/charge_start_threshold"
+        local _threshold_stop="${SYS_POWERSUPPLY}/${_bat}/charge_stop_threshold"
+
+        if [ -e "${_threshold_start}" ] && [ -e "${_threshold_stop}" ]; then
+            printf " [%s%%-%s%%]" "$(cat "${_threshold_start}")" "$(cat "${_threshold_stop}")"
+        fi
+    }
+
+    __time_to_full() {
+        local _threshold_file="${SYS_POWERSUPPLY}/${_bat}/charge_stop_threshold"
+        local _capacity_target
+        if [ -e "${_threshold_file}" ]; then
+            _capacity_target="$(__calc "($(cat "${_threshold_file}") / 100) * ${_capacity_full}")"
+        else
+            _capacity_target="${_capacity_full}"
+        fi
+
+        local _time
+        _time="$(__calc "60 * (${_capacity_target} - ${_capacity_now}) / ${_power}")"
+
+        local _perc_per_ten
+        _perc_per_ten="$(__calc "100 * (1/6) * ${_power} / ${_capacity_full}")"
+
+        printf "(%.1fmin; %.1f%%/10min)" "${_time}" "${_perc_per_ten}"
+    }
+
+    local _ac=""
+    if [ "$(cat "${SYS_POWERSUPPLY}/AC/online")" = "1" ]; then
+        _ac="yes"
+    fi
+    if [ "${_ac}" ]; then
+        printf "AC/"
+    else
+        printf "BAT/"
+    fi
+
+    __battery_id
+    case "$(cat "${SYS_POWERSUPPLY}/${_bat}/status")" in
+        "Not charging")
+            printf "==%s" "$(__perc)"
+            if [ "${_ac}" ]; then
+                __threshold
+            fi
+            ;;
+        "Charging")
+            __perc
+            __threshold
+            if ! __in_use; then
+                printf "  (updating powerdraw)"
+            else
+                printf "  @+%s" "$(__powerdraw)"
+                printf " ~%s" "$(__time_to_full)"
+            fi
+            ;;
+        "Discharging")
+            __perc
+            if ! __in_use; then
+                printf "  (updating powerdraw)"
+            else
+                printf "  @-%s" "$(__powerdraw)"
+                printf " ~%s" "$(__time_to_empty)"
+            fi
+            ;;
+    esac
+    printf "  // %s" "$(__health)"
+}
+
+__status() {
+    if [ ! -e "${SYS_POWERSUPPLY}/AC" ]; then
+        printf "power> desktop\n"
+        return
+    fi
+
+    find "${SYS_POWERSUPPLY}" -mindepth 1 -printf "%P\n" | sort -n | grep "^BAT" | while read -r _bat; do
+        __status_one "${_bat}"
+        printf "\n"
+    done
+}
+
+case "${1}" in
+    "set")
+        shift
+        __base
+        set_threshold "${@}"
+        ;;
+    *)
+        __status
+        ;;
+esac
